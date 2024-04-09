f2edecc9a585261cfa862c927d2ef0f9499ac9c3
kevinkarikalan <kevinkarikalan93@gmail.com>
2024-04-09 15:36:41 +0530

    react file

---
+++ one.jsx
@@ -0,0 +1,14 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class one extends Component {
+  render() {
+    return (
+      <View>
+        <Text>one</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
